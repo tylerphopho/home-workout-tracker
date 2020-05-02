92cc0c4941e6ad1cbea2caf62d9abae1e3903010
tylerphopho <tyler.phopho@gmail.com>
2020-05-02 11:28:05 -0600

    finished setting up modals

---
+++ models/Workout.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    name: {
+        type: String,
+        trim: true
+    },
+    created: {
+        type: Date,
+        default: Date
+    },
+    exercises: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Exercise"
+        }
+    ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
