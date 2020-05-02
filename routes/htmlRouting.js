ebef385e471e557d5e057a8f90b5b0f43cdd1e5e
tylerphopho <tyler.phopho@gmail.com>
2020-05-02 17:08:05 -0600

    App works!

--- routes/htmlRouting.js
+++ routes/htmlRouting.js
@@ -2,9 +2,9 @@ const path = require("path");
 
 module.exports = function(app){
     app.get("/exercise", (req, res) => {
-        res.sendFile(path.join(__dirname, "../Develop/public/exercise.html"));
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
     app.get("/stats", (req, res) => {
-        res.sendFile(path.join(__dirname, "../Develop/public/stats.html"))
+        res.sendFile(path.join(__dirname, "../public/stats.html"))
     })
 }
